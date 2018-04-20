7ec81320e8c1f944ab4c02046075ce61eae39f9f
marcdvlpr <marcdvlpr@gmail.com>
2018-04-20 14:37:48 +0200

    Add DB SQL

---
+++ config.js
@@ -0,0 +1,15 @@
+let config = {
+	database: {
+		host:	  'localhost', 	// database host
+		user: 	  'root', 		// your database username
+		password: 'wildcodeschool2018', 		// your database password
+		port: 	  3306, 		// default MySQL port
+		db: 	  'edual' 		// your database name
+	},
+	server: {
+		host: '127.0.0.1',
+		port: '3000'
+	}
+}
+
+module.exports = config
